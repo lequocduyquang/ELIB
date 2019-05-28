e228a9ce452fbfe7375d034431171c916cc93ed6
lequocduyquang <duyquangbtx@gmail.com>
2019-05-28 17:04:03 +0700

    setup views

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.render('home')
 })
 
 app.listen(PORT, () => console.log('App running on port 3000'))
