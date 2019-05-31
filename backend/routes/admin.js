920ccf3f6378bd72c1d8abc88595b5853348acc0
lephamhoangquy <hoangquy103@gmail.com>
2019-05-31 18:25:19 +0700

    add addnewbook form

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('layouts/admin/admin', {
-        layout: false
+        layout: false,
+    })
+});
+
+router.get('/addnewbook', (req, res) => {
+    res.render('layouts/admin/admin', {
+        layout: false,
+        addnewbook:true
     })
 });
 
