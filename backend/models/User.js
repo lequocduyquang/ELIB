6b0f66bd4424253666f56001d56975db1ac52a98
lephamhoangquy <hoangquy103@gmail.com>
2019-06-05 17:27:18 +0700

    update admin

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,10 +14,4 @@ const UserSchema = new Schema({
     ]
 })
 
-// Custom validation for email
-UserSchema.path('email').validate((val) => {
-    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(val);
-}, 'Invalid e-mail.');
-
 module.exports = mongoose.model('User', UserSchema)
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,14 +4,37 @@ var router = express.Router();
 router.get('/', (req, res) => {
     res.render('layouts/admin/admin', {
         layout: false,
+        profile: true,
+        title: 'User Profile',
+        isActiveProfile:true
     })
 });
 
 router.get('/addnewbook', (req, res) => {
     res.render('layouts/admin/admin', {
         layout: false,
-        addnewbook:true
+        addnewbook: true,
+        title: 'Add new book',
+        isActiveAdd:true
     })
 });
+
+router.get('/profile', (req, res, next) => {
+    res.render('layouts/admin/admin', {
+        layout: false,
+        profile: true,
+        title: 'User Profile',
+        isActiveProfile:true
+    })
+})
+
+router.get('/listbook',(req,res,next)=>{
+    res.render('layouts/admin/admin',{
+        layout:false,
+        listbook:true,
+        title: 'Table',
+        isActiveList:true
+    })
+})
 
 module.exports = router;
